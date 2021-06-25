9886e4b1abd086afb09545e2c26a53686929cc1c
miketheodorou <miketheodorou@lectrefy.com>
2021-06-24 18:34:21 -0600

    updates repo detail page

--- src/pages/RepositoryDetail/RepositoryDetail.tsx
+++ src/pages/RepositoryDetail/RepositoryDetail.tsx
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query';
 
 import { getRepo } from '../../api/RepoAPI';
 
-import { Container, Icon, Popup } from 'semantic-ui-react';
+import { Icon, Loader, Popup } from 'semantic-ui-react';
 import { Count } from '../../components';
 
 import { Repository } from '../../utils/models/Repository';
@@ -17,13 +17,12 @@ interface RepositoryRouteParams {
 
 const RepositoryDetail = () => {
   const { owner: ownerParam, repo } = useParams<RepositoryRouteParams>();
-  const { data, isLoading, isError } = useQuery<Repository, Error>([ownerParam, repo], () =>
+  const { data, isLoading, error } = useQuery<Repository, Error>([ownerParam, repo], () =>
     getRepo(ownerParam, repo)
   );
-  console.log('data', data);
 
-  if (!data) {
-    return null;
+  if (isLoading) {
+    return <Loader active={isLoading} />;
   }
 
   const {
@@ -31,71 +30,76 @@ const RepositoryDetail = () => {
     description,
     owner,
     html_url,
-    stargazers_count,
-    subscribers_count,
-    forks_count,
-    open_issues_count,
-  } = data;
+    stargazers_count = 0,
+    subscribers_count = 0,
+    forks_count = 0,
+    open_issues_count = 0,
+  } = data || {};
+
+  const { avatar_url, login } = owner || {};
 
   return (
-    <Container text className='repo'>
-      <article className='ui padded segment'>
-        <header>
-          <div className='repo__title-container'>
-            <h1 className='repo__title'>{name}</h1>
+    <main className='repo'>
+      {error && <p>{error.message}</p>}
+      {data && (
+        <article className='ui padded segment'>
+          <header>
+            <div className='repo__title-container'>
+              <h1 className='repo__title'>{name}</h1>
 
-            <div className='repo__links'>
-              <Popup
-                content='Back to search'
-                trigger={
-                  <Link to='/' aria-label='Back to search'>
-                    <Icon name='arrow left' />
-                  </Link>
-                }
-              />
-              <Popup
-                content='View on Github'
-                trigger={
-                  <a href={html_url} target='_blank' rel='noreferrer' aria-label='View on Github'>
-                    <Icon name='github' />
-                  </a>
-                }
-              />
+              <div className='repo__links'>
+                <Popup
+                  content='Back to search'
+                  trigger={
+                    <Link to='/' aria-label='Back to search'>
+                      <Icon name='arrow left' />
+                    </Link>
+                  }
+                />
+                <Popup
+                  content='View on Github'
+                  trigger={
+                    <a href={html_url} target='_blank' rel='noreferrer' aria-label='View on Github'>
+                      <Icon name='github' />
+                    </a>
+                  }
+                />
+              </div>
             </div>
-          </div>
 
-          <div className='repo__owner'>
-            <img
-              className='ui avatar image'
-              width='50'
-              height='50'
-              src={owner.avatar_url}
-              alt={owner.login}
+            <div className='repo__owner'>
+              <img
+                className='ui avatar image'
+                width='50'
+                height='50'
+                src={avatar_url}
+                alt={login}
+              />
+              <span>{login}</span>
+            </div>
+          </header>
+
+          <p>{description}</p>
+
+          <div className='repo__counts'>
+            <Count className='ui label large' count={stargazers_count} title='Stars' icon='star' />
+            <Count className='ui label large' count={forks_count} title='Forks' icon='sitemap' />
+            <Count
+              className='ui label large'
+              count={subscribers_count}
+              title='Subscribers'
+              icon='eye'
             />
-            <span>{owner.login}</span>
+            <Count
+              className='ui label large'
+              count={open_issues_count}
+              title='Issues'
+              icon='bullseye'
+            />
           </div>
-        </header>
-
-        <p>{description}</p>
-
-        <div className='repo__counts'>
-          <Count className='ui label large' count={stargazers_count} title='Stars' icon='star' />
-          <Count className='ui label large' count={forks_count} title='Forks' icon='sitemap' />
-          <Count
-            className='ui label large'
-            count={subscribers_count}
-            title='Subscribers'
-            icon='eye'
-          />
-          <Count
-            className='ui label large'
-            count={open_issues_count}
-            title='Issues'
-            icon='bullseye'
-          />
-        </div>
-      </article>
-    </Container>
+        </article>
+      )}
+    </main>
   );
 };
 
